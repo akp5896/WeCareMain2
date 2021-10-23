b7b419046e49826e1747d04f0da2be4e109a674f
akp5896 <akp5896@psu.edu>
2021-10-22 20:28:31 -0400

    added menu

--- app/src/main/java/com/example/wecaremain/MainActivity.java
+++ app/src/main/java/com/example/wecaremain/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.wecaremain;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_favorites:
+                        // do something here
+                        Log.i("123","123");
+                        return true;
+                    case R.id.action_schedules:
+                        // do something here
+                        Log.i("123","456");
+                        return true;
+                    case R.id.action_music:
+                        // do something here
+                        Log.i("123","789");
+                        return true;
+                    default: return true;
+                }
+            }
+        });
+
     }
 }
