e4286e655de4c56945c56ed68870817cf2e1edee
Shyam Pillai <shyam.b.pillai@gmail.com>
2021-10-23 02:05:03 -0400

    VentCompose

---
+++ app/src/main/java/com/example/wecaremain/ventCompose.java
@@ -0,0 +1,14 @@
+package com.example.wecaremain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ventCompose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vent_compose);
+    }
+}
