f3feaef1c5616df89c6dd3cbf0e2e8d6d4e709df
Shyam Pillai <shyam.b.pillai@gmail.com>
2021-10-24 09:33:24 -0400

    v1gettingstarted

---
+++ app/src/main/java/com/example/wecaremain/gettingStarted.java
@@ -0,0 +1,32 @@
+package com.example.wecaremain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class gettingStarted extends AppCompatActivity {
+
+    private Button getStartedButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started);
+
+        getStartedButton = (Button) findViewById(R.id.getStartedButton);
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLogin();
+            }
+    });
+}
+
+    private void openLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+    }
